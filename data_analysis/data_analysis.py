02015d68bb6010146a41e034cfbed728b99d290d
Niklas Egebak Joensen <joensenniklas@gmail.com>
2023-02-21 14:02:11 +0100

    additional exploratory work. Added library directory. Figured out how to change time units, created seconds_to() as a generalized solution.

--- data_analysis/data_analysis.py
+++ data_analysis/data_analysis.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import math
 import matplotlib.pyplot as plt
+import libs.libraries
+from enum import Enum
 
 data_location = 'D:/unidata/data.ukedc.rl.ac.uk/browse/edc/efficiency/residential/EnergyConsumption/Domestic/UK-DALE-2017/UK-DALE-FULL-disaggregated/ukdale'
 # contains house 1-5 + metadata folder.
@@ -11,11 +13,33 @@ house1_channel_6 = 'D:/unidata/data.ukedc.rl.ac.uk/browse/edc/efficiency/residen
 house1_channel_7 = 'D:/unidata/data.ukedc.rl.ac.uk/browse/edc/efficiency/residential/EnergyConsumption/Domestic/UK-DALE-2017/UK-DALE-FULL-disaggregated/ukdale/house_1/channel_7.dat'
 #Read disaggregated data
 
+def seconds_to(unit : str, unix_seconds : int) -> int: 
+    shrinkfactor_dict = {
+        "minutes": 60,
+        "5-minutes": 300,
+        "quarters": 900,
+        "half-hours": 1600,
+        "hours": 3600
+    }
+    return unix_seconds/shrinkfactor_dict.get(unit)
+
+def write_dataframe_to_csv(dataframe : pd.DataFrame, filename : str) -> None: 
+    return
+    #todo 
+
 def unix_secs_to_unix_mins(unix_time : int) -> int: 
     return math.floor(unix_time / 60)
 
+def unix_secs_to_fifteen_min(unix_time : int): return math.floor(unix_time / 900)
+
+# Full outer join on two appliance dataframes
+def combine_dataframes(df1 : pd.DataFrame, df2 : pd.DataFrame) -> pd.DataFrame:
+    df3 = pd.merge(df1, df2, how='outer', on='Time') 
+    return df3
+
 
 def read_x_entries(x : int, channel_file : str) -> pd.DataFrame:
+    # TODO : Refactor this code
     lines = []
 
     with open(channel_file) as data:
@@ -40,13 +64,13 @@ def plot_power_usage(dataframe : pd.DataFrame) -> None:
 def main():
     data = read_x_entries(1000, house1_channel_4)
     minute_data = get_average_minute_consum(data)
+    #minute_data_2 = get_average_minute_consum(read_x_entries(1000, house1_channel_5))
     #print(minute_data)
-    print(len(minute_data.index))
+    #combined_frame = combine_dataframes(minute_data, minute_data_2)
     plot_power_usage(minute_data)
 
 
 main()
-
 
 
 
---
+++ data_analysis/libs/libraries.py
@@ -0,0 +1,2 @@
+def hiprinter():
+    print("hi :D")
