8c33c1bc95d1fa2a13e77eb6a4cb766ad786a888
Niklas Egebak Joensen <joensenniklas@gmail.com>
2023-02-28 09:58:42 +0100

    intermediate push

--- data_analysis/data_analysis.py
+++ data_analysis/data_analysis.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 import libs.libraries
 from enum import Enum
 import sys
+import os
+import re as regex
 
 #TODO better data access solution
 #TODO refactoring of tools and helper functions into library package
@@ -16,12 +18,13 @@ import sys
 
 #make variable string
 dataset = str(sys.argv[1])
-house_1 = dataset + "house_1"
-house_2 = dataset + "house_2"
-house_3 = dataset + "house_3"
-house_4 = dataset + "house_4"
-house_5 = dataset + "house_5"
+house_1 = dataset + "/house_1"
+house_2 = dataset + "/house_2"
+house_3 = dataset + "/house_3"
+house_4 = dataset + "/house_4"
+house_5 = dataset + "/house_5"
 
+# find filer med channel_(int) i regex
 
 def seconds_to(unit : str, unix_seconds : int) -> int: 
     shrinkfactor_dict = {
@@ -46,7 +49,16 @@ def unix_secs_to_fifteen_min(unix_time : int): return math.floor(unix_time / 900
 def combine_dataframes(df1 : pd.DataFrame, df2 : pd.DataFrame) -> pd.DataFrame:
     df3 = pd.merge(df1, df2, how='outer', on='Time') 
     return df3
+ 
 
+def get_average_minute_consum(entries_in_seconds : pd.DataFrame) -> pd.DataFrame:
+    return entries_in_seconds.groupby('Time').mean()
+
+def plot_power_usage(dataframe : pd.DataFrame) -> None:
+     
+    dataframe.plot.bar()
+    dataframe.plot()
+    plt.show()
 
 def read_x_entries(x : int, channel_file : str) -> pd.DataFrame:
     # TODO : Refactor this code
@@ -60,24 +72,30 @@ def read_x_entries(x : int, channel_file : str) -> pd.DataFrame:
             lines.append(line)
     
     return pd.DataFrame(lines, columns=['Time', 'Power'])
- 
 
-def get_average_minute_consum(entries_in_seconds : pd.DataFrame) -> pd.DataFrame:
-    return entries_in_seconds.groupby('Time').mean()
 
-def plot_power_usage(dataframe : pd.DataFrame) -> None:
-     
-    dataframe.plot.bar()
-    dataframe.plot()
-    plt.show()
+def get_data_from_house():
+    #res df
+    result_frame = pd.DataFrame({'Time':[], 'Power':[]})
+    for file in os.listdir(house_3):
+        if (regex.compile("/channel_[0-9]+.dat").match(file)):
+            # read file from data and return dataframe
+            temp = read_x_entries(1000, file)
+            temp = get_average_minute_consum(temp)
+            #join temp on res
+            combine_dataframes(result_frame, temp)
+    return result_frame
+
 
 def main():
-    data = read_x_entries(1000, house1_channel_4)
-    minute_data = get_average_minute_consum(data)
+    #data = read_x_entries(1000, house1_channel_4)
+    #minute_data = get_average_minute_consum(data)
     #minute_data_2 = get_average_minute_consum(read_x_entries(1000, house1_channel_5))
     #print(minute_data)
     #combined_frame = combine_dataframes(minute_data, minute_data_2)
-    plot_power_usage(minute_data)
+    #plot_power_usage(minute_data)
+    house_3_data = get_data_from_house()
+    print(house_3_data)
 
 
 main()
