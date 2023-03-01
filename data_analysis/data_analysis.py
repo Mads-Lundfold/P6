92fcee207798340da15ed47bd82f66de0ec7b3cf
MadsLundfold <70686308+MadsLundfold@users.noreply.github.com>
2023-03-01 09:30:42 +0100

    Initial code cleaning
    - Removed redundant functions.
    
    - Made certain functionalities easier to call.

--- data_analysis/data_analysis.py
+++ data_analysis/data_analysis.py
@@ -26,7 +26,7 @@ house_5 = dataset + "/house_5"
 
 # find filer med channel_(int) i regex
 
-def seconds_to(unit : str, unix_seconds : int) -> int: 
+def convert_seconds_to(time : int, to : str) -> int: 
     shrinkfactor_dict = {
         "minutes": 60,
         "5-minutes": 300,
@@ -34,35 +34,21 @@ def seconds_to(unit : str, unix_seconds : int) -> int:
         "half-hours": 1600,
         "hours": 3600
     }
-    return unix_seconds/shrinkfactor_dict.get(unit)
+    return math.floor(time / shrinkfactor_dict.get(to))
 
 def write_dataframe_to_csv(dataframe : pd.DataFrame, filename : str) -> None: 
+    # TODO
     return
-    #todo 
+    
 
-def unix_secs_to_unix_mins(unix_time : int) -> int: 
-    return math.floor(unix_time / 60)
-
-def unix_secs_to_fifteen_min(unix_time : int): return math.floor(unix_time / 900)
-
-# Full outer join on two appliance dataframes
-def combine_dataframes(df1 : pd.DataFrame, df2 : pd.DataFrame) -> pd.DataFrame:
-    df3 = pd.merge(df1, df2, how='outer', on='Time') 
-    return df3
-
-def join_dataframes(right_df : pd.DataFrame, left_df : pd.DataFrame):
-    return right_df.join(left_df, on='Time')
-
-
-
-def get_average_minute_consum(entries_in_seconds : pd.DataFrame) -> pd.DataFrame:
+def get_average_consumption(entries_in_seconds : pd.DataFrame) -> pd.DataFrame:
     return entries_in_seconds.groupby('Time').mean()
 
+
 def plot_power_usage(dataframe : pd.DataFrame) -> None:
-     
-    dataframe.plot.bar()
     dataframe.plot()
     plt.show()
+
 
 def read_x_entries(x : int, channel_file : str) -> pd.DataFrame:
     # TODO : Refactor this code
@@ -71,7 +57,8 @@ def read_x_entries(x : int, channel_file : str) -> pd.DataFrame:
     with open(channel_file) as data:
         for _ in range(x):
             line = next(data).split()
-            line[0] = (unix_secs_to_fifteen_min(int(line[0]))) # Does 2 things at once! calculates and converts from string to int.
+            time = int(line[0])
+            line[0] = convert_seconds_to(time, to='quarters') # Does 2 things at once! calculates and converts from string to int.
             line[1] = int(line[1])
             lines.append(line)
     
@@ -84,7 +71,7 @@ def read_entries_from(channel_file : str):
     with open(channel_file) as file:
         for line in file:
             split_list = line.split()
-            split_list[0] = unix_secs_to_unix_mins(int(split_list[0]))
+            split_list[0] = convert_seconds_to(int(split_list[0]), 'quarters')
             split_list[1] = int(split_list[1])
             lines.append(split_list)
     
@@ -92,14 +79,13 @@ def read_entries_from(channel_file : str):
 
 
 def get_data_from_house(house_number : str):
-    #res df
     result_frame = list()
 
     for file in os.listdir(house_number):
         if (regex.compile("channel_[0-9]+.dat").match(file)):
             # read file from data and return dataframe
             temp = read_entries_from(house_number + '/' + file)
-            temp = get_average_minute_consum(temp)
+            temp = get_average_consumption(temp)
             #join temp on res
             result_frame.append(temp)
     
@@ -107,16 +93,15 @@ def get_data_from_house(house_number : str):
     return result_frame
 
 
+def convert_watt_df_to_binary(watt_dataframe : pd.DataFrame):
+    return watt_dataframe.where(watt_dataframe == 0, 1)
+
+
 def main():
-    #data = read_x_entries(1000, house1_channel_4)
-    #minute_data = get_average_minute_consum(data)
-    #minute_data_2 = get_average_minute_consum(read_x_entries(1000, house1_channel_5))
-    #print(minute_data)
-    #plot_power_usage(minute_data)
-    house_data = get_data_from_house(house_number = house_3)
-    house_data.where(house_data == 0, 1, inplace=True)
-    print(house_data)
-    #print(house_3_data)
+    watt_house_data = get_data_from_house(house_number = house_3)
+    binary_house_data = convert_watt_df_to_binary(watt_house_data)
+    print(watt_house_data)
+    print(binary_house_data)
 
 
 main()
