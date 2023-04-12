a1622feacad56e3589ee69d70d78294d79466ad9
Mads-Lundfold <lundfold.business@gmail.com>
2023-04-12 11:16:28 +0200

    Simplified process of finding usage frequencies

--- data_analysis/time_associations.py
+++ data_analysis/time_associations.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import sys
+import datetime
 
 from data_analysis import get_data_from_house
 
@@ -21,53 +22,18 @@ QUARTERS_IN_DAY = 24*4 #96 quarters in a day
 
 
 # Counts how often each appliance is used at each quater of each day
-def usage_frequencies(on_off_df: pd.DataFrame):
+def usage_frequencies(df: pd.DataFrame):
 
-    # Convert bool values to int (0,1)
-    on_off_df = on_off_df.astype(int)
+    # Convert values in dataframe from boolean to int (0,1)
+    df = df.astype(int)
 
-    # Split full dataframe into list of daily dataframes
-    chunk_list = chunkify(on_off_df, chunk_size=QUARTERS_IN_DAY*900)
+    # Convert unix timestamps to datetime hour:minute:seconds
+    df.index = pd.to_datetime(df.index, unit='s').time
 
-    # Add each daily dataframe together to get how often the appliances were used at each quater of the day
-    freq = chunk_list[0]
+    # Groupby time index to get frequencies
+    frequencies = df.groupby(df.index).sum()
 
-    for i in range(1, len(chunk_list)):
-        freq = freq.add(chunk_list[i], fill_value=0)
-
-    return freq
-
-
-# Slices bigger dataframes into daily dataframes
-def chunkify(df: pd.DataFrame, chunk_size: int):
-
-    # Slice dataframe into daily dataframes
-    # chunk_list = [df.loc[i : i+chunk_size] for i in range(df.index[0], df.index[-1], chunk_size)]
-
-    chunk_list = list()
-    start = df.index[0]
-    chunk_indexes = list()
-
-    for index in df.index:
-        if index < (start + chunk_size):
-            chunk_indexes.append(index)
-        else:
-            chunk_list.append(df.loc[chunk_indexes])
-            start += chunk_size
-            chunk_indexes.clear()
-            chunk_indexes.append(index)
-
-    
-    # Change 'time' column for each chunk from the unix timestamp to the time of the given day.
-    # The first time (00:00) should be 0, whereas the last time (23:45) should be 86400, as that's the amount of seconds from the start.
-    for chunk in chunk_list:
-        if len(chunk) > 0:
-            start = chunk.index[0]
-        
-            for i in range(0, len(chunk)):
-                chunk.rename(index={chunk.index[i] : (chunk.index[i] - start)}, inplace=True)
-
-    return chunk_list
+    return frequencies
 
 
 # Make a chart for the usage frequency of each appliance.
@@ -80,6 +46,6 @@ def plot_frequencies(df: pd.DataFrame):
 
 # Running it
 watt_df, on_off_df = get_data_from_house(house_number = house_3) 
-frequencies = usage_frequencies(on_off_df=on_off_df)
+frequencies = usage_frequencies(on_off_df)
 print(frequencies)
-#plot_frequencies(frequencies)
+plot_frequencies(frequencies)
