de4d11dd0c280a232e0595a447af28e6cd5b80b2
Niklas Egebak Joensen <joensenniklas@gmail.com>
2023-04-25 13:10:26 +0200

    added functionality to use event profiles with variable element duration and profile size.

--- data_analysis/optimizer.py
+++ data_analysis/optimizer.py
@@ -1,6 +1,9 @@
 # !!!READ!!! - Entirely unfinished. Lots of thought has been put in, but currently only does the absolute simplest thing it can - it returns the lowest price in a world where everything is hourly and constant.
 # Much of the code is junk and has little effect, but the idea was to expand upon it. This should happen soon.
 
+# need to modify algorithm such that granularity other than hours can be used.
+    # idea: use units_in_minutes from input event to modify tariff vector
+
 from calendar import timegm
 import pandas as pd
 import time
@@ -8,12 +11,8 @@ from datetime import datetime
 from events import FakeDiscreteLvl1Event
 from electricity_price_dataset import read_extract_convert_price_dataset
 
-thingy_session = FakeDiscreteLvl1Event(profile = [0.3],
-                               units_in_minutes = 60, 
-                               maxfreq = 1, 
-                               minfreq = 1, 
-                               restricted_hours = [(22, 8), (11, 12)],
-                               occured=datetime(2015, 11, 20, 18, 0, 0))
+HOUR_IN_MINUTES = 60
+
 
 def optimize(event: FakeDiscreteLvl1Event, price_data: pd.DataFrame, start_time: datetime, end_time: datetime):
     # Cut power data to fit optimization period.
@@ -30,20 +29,67 @@ def optimize(event: FakeDiscreteLvl1Event, price_data: pd.DataFrame, start_time:
 
     price_vector = cut_price_data["GB_GBN_price_day_ahead"].tolist()
     price_vector = list(map(float, price_vector))
+
+    # Change granularity of price vector
+    expansion_factor = calculate_expansion_factor(event.units_in_minutes)
+
+    price_vector = expand_price_vector(price_vector, expansion_factor)
     print(price_vector)
+
+    print(f"lowest element in price vector: {min(price_vector)}")
+    print(f"lowest element in price vector times 4: {min(price_vector*4)}")
 
     # Perform optimization
         # we need to find a way to map our appliance to a point on the price vector. For now we just place it on the 18th hour.
-    previous_cost = event.profile[0]*price_vector[18]
+    previous_cost = get_cost_of_single_timeslot(event.profile, price_vector, timeslot_start=10) #TODO timeslot_start is temp bad solution.
+    print(f"previous cost {previous_cost}")
         # Now have current price. Need new price, savings, new time.
         # we will now iterate the simplest way possible. it's all hourly. For now we will even ignore restricted hours.
     lowest_cost = previous_cost
-    for timeslot in price_vector:
-        if(timeslot*event.profile[0] < lowest_cost): 
-            lowest_cost = timeslot*event.profile[0]
 
+    for i in range(len(price_vector)-len(event.profile)):
+        new_cost = get_cost_of_single_timeslot(event.profile, price_vector, timeslot_start=i)
+        if(new_cost < lowest_cost): 
+            lowest_cost = new_cost
+
+    print(f"lowest cost: {lowest_cost}")
     return lowest_cost # needs to return more information - at what time do you get the cost? how expensive was observed use? How much was saved?
 
+def calculate_expansion_factor(event_units_in_minutes):
+    expansion_factor = int(HOUR_IN_MINUTES//event_units_in_minutes)
+    if (expansion_factor < 1): 
+        print("Expansion factor less than 1 in optimize(), exiting.")
+        exit(1)
+    elif (not(expansion_factor % HOUR_IN_MINUTES)):
+        print("Expansion factor not divisible by 60 in optimize, exiting.")
+        exit(1)
+    return expansion_factor
+
+def expand_price_vector(price_vector: list, expansion_factor: int) -> list:
+    # create new list with each value repeating price_vector many times.
+    expanded_price_vector = list()
+    for element in price_vector:
+        for i in range(expansion_factor):
+            expanded_price_vector.append(element)
+
+    return expanded_price_vector
+            
+#TODO currently tomeslot_start is just an element in the price vector. For now is simple solution.
+def get_cost_of_single_timeslot(event_profile: list(), price_vector: list(), timeslot_start: int)-> float:
+    cost_sum = 0
+    i = 0
+    for element in event_profile:
+        cost_sum = cost_sum + element*price_vector[timeslot_start + i]
+        i = i + 1
+    return cost_sum
+
+
+thingy_session = FakeDiscreteLvl1Event(profile = [1, 1, 1, 1, 1],
+                               units_in_minutes = 15, 
+                               maxfreq = 1, 
+                               minfreq = 1, 
+                               restricted_hours = [(22, 8), (11, 12)],
+                               occured=datetime(2015, 11, 20, 18, 0, 0))
 
 start_time = datetime(2015, 11, 20, 0, 0, 0)
 end_time = datetime(2015, 11, 21, 0, 0, 0)
