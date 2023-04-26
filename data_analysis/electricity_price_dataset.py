526d48a670ed9a7edeedb7904fc7e8ddd62355c5
Vikuizmas <vzukau20@student.aau.dk>
2023-04-26 09:58:50 +0200

    Fixed time units, fixed MWh to Wh and new dataset.

--- data_analysis/electricity_price_dataset.py
+++ data_analysis/electricity_price_dataset.py
@@ -3,16 +3,16 @@ import pandas as pd
 def read_extract_convert_price_dataset():
 
     # Read the CSV file into a DataFrame
-    df = pd.read_csv("data\Electricity_price_dataset.csv", header=None, names=["utc_timestamp", "cet_cest_timestamp", "GB_GBN_price_day_ahead"], delimiter=";", skiprows=[1])
-
+    df = pd.read_csv("data\Electricity_price_dataset_new.csv", header=None, names=["utc_timestamp", "cet_cest_timestamp", "GB_GBN_price_day_ahead"], delimiter=",", skiprows=[1])
+    
     # Extract only col1 and col3 columns
     df = df[["utc_timestamp", "GB_GBN_price_day_ahead"]]
 
-    # Convert col1 from UTC to Unix timestamp
+    # Convert col1 from ISO 8601 to Unix timestamp
     timestamps = []
     for timestamp_str in df["utc_timestamp"]:
         try:
-            timestamp = pd.to_datetime(timestamp_str, format="%d-%m-%Y %H:%M")
+            timestamp = pd.to_datetime(timestamp_str)
             timestamp_unix = int(timestamp.timestamp())
             timestamps.append(timestamp_unix)
         except ValueError:
@@ -24,6 +24,9 @@ def read_extract_convert_price_dataset():
 
     # Rename the first column to "unix_timestamp"
     df = df.rename(columns={"utc_timestamp": "unix_timestamp"})
+
+    # Convert GB_GBN_price_day_ahead from string to float, then to integer and then into WH from MWh
+    df["GB_GBN_price_day_ahead"] = df["GB_GBN_price_day_ahead"].astype(float).astype(int) / 1000000
 
     return df
 
