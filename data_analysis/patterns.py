d2474a250ea0eef2e7d916058bc188b7cbc6a1d9
Mads-Lundfold <lundfold.business@gmail.com>
2023-05-11 10:53:25 +0200

    Added naive functionality for creating level 2 patterns from the output json file from TPM

--- data_analysis/patterns.py
+++ data_analysis/patterns.py
@@ -4,6 +4,7 @@
 
 import pandas as pd
 import re
+from datetime import datetime
 
 # Class for pattern objects. We make objects so they are easier to work with.
 class Level2Pattern:
@@ -44,10 +45,48 @@ def pattern_extraction(json_file_path: str):
     return patterns
 
 
-def main():
-    patterns = pattern_extraction('./TPM/TPM/output/Experiment_minsup0.15_minconf_0.6/level2.json')
+class Patterns:
+    def __init__(self, date, appliances, relation, start_times):
+        self.date = date
+        self.appliances = appliances
+        self.relation = relation
+        self.start_times = start_times
+    
+    def print(self):
+        print(f'{self.date} | {self.appliances} | {self.relation} | {self.start_times}')
 
+
+def optimization_patterns(json_file_path: str):
+    extracted_data = pd.read_json(json_file_path)
+    print(extracted_data)
+
+    patterns = list()
+
+    for data in extracted_data['patterns']:
+        for pattern in data:
+            appliances_and_relation = re.split('(>|->|\|)', pattern['pattern'])
+            appliances = (appliances_and_relation[0], appliances_and_relation[2])
+            relation = appliances_and_relation[1]
+            for key in pattern['time']:
+                for occurence in pattern['time'][key]:
+                    date = datetime.strptime(occurence[0][0], '%Y-%m-%d %H:%M:%S').strftime('%m-%d')
+                    start_times = (datetime.strptime(occurence[0][0], '%Y-%m-%d %H:%M:%S').strftime('%H:%M:%S'), 
+                                   datetime.strptime(occurence[1][0], '%Y-%m-%d %H:%M:%S').strftime('%H:%M:%S'))
+                    
+                    patterns.append(Patterns(date, appliances, relation, start_times))
+    
+    patterns.sort(key=lambda pattern: pattern.date)
+
+    return patterns
+
+def main():
+    patterns = optimization_patterns('./TPM/TPM/output/Experiment_minsup0.15_minconf_0.6/level2.json')
+    for p in patterns:
+        p.print()
+
+    '''
     for pattern in patterns:
         pattern.print()
+    '''
 
 main()
