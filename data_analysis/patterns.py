a27df575e828eeef1ba009ff1b691fcce7fe0b98
Mads-Lundfold <lundfold.business@gmail.com>
2023-05-11 12:50:07 +0200

    Removed unused legacy code

--- data_analysis/patterns.py
+++ data_analysis/patterns.py
@@ -6,45 +6,6 @@ import pandas as pd
 import re
 from datetime import datetime
 
-'''
-# Class for pattern objects. We make objects so they are easier to work with.
-class Level2Pattern:
-    def __init__(self, first_appliance, second_appliance, relation, support, confidence):
-        self.first_appliance = first_appliance
-        self.second_appliance = second_appliance
-        self.relation = relation
-        self.support = support
-        self.confidence = confidence
-    
-    def print(self):
-        print(f'{self.first_appliance} {self.relation} {self.second_appliance}, support: {self.support}, confidence: {self.confidence}')
-
-
-def pattern_extraction(json_file_path: str):
-    # Read JSON file and convert to pandas dataframe
-    extracted_data = pd.read_json(json_file_path)
-    
-    patterns = list()
-
-    # Iterate through each pattern in the dataframe
-    for data in extracted_data['patterns']:
-        for pattern in data:
-            # Split the information about each pattern 
-            appliances_and_relation = re.split('(>|->|\|)', pattern['pattern'])
-            first_appliance = appliances_and_relation[0]
-            second_appliance = appliances_and_relation[2]
-            relation = appliances_and_relation[1]
-            support = pattern['supp']
-            confidence = pattern['conf']
-            
-            # Create pattern object
-            level_2_pattern = Level2Pattern(first_appliance, second_appliance, relation, support, confidence)
-            
-            # Append pattern to list of patterns
-            patterns.append(level_2_pattern)
-
-    return patterns
-'''
 
 # Class for pattern objects. We make objects so they are easier to work with.
 class Patterns:
@@ -78,6 +39,7 @@ def optimization_patterns(json_file_path: str):
                         
                     patterns.append(Patterns(date, appliances, relation, start_times))
     
+    # Sort the patterns based on date
     patterns.sort(key=lambda pattern: pattern.date)
 
     return patterns
@@ -87,10 +49,5 @@ def main():
     patterns = optimization_patterns('./TPM/TPM/output/Experiment_minsup0.15_minconf_0.6/level2.json')
     for p in patterns:
         p.print()
-
-    '''
-    for pattern in patterns:
-        pattern.print()
-    '''
 
 main()
