5a8b471ea1380d7a3f13e43e67edecc0043ec337
Niklas Egebak Joensen <joensenniklas@gmail.com>
2023-05-12 09:03:45 +0200

    changed filter_level_2_events() to return list of lvl 2 pattern dicts.

--- data_analysis/level_2_filter.py
+++ data_analysis/level_2_filter.py
@@ -1,6 +1,7 @@
 from patterns import Patterns
 from event_factory import Event
 from datetime import datetime
+from typing import List
 
 def do_events_match(pattern: Patterns, level_1_event: Event)-> bool:
     result = False
@@ -9,15 +10,22 @@ def do_events_match(pattern: Patterns, level_1_event: Event)-> bool:
     for i in range(number_of_appliances):
         if (datetime.strptime(pattern.appliance_start_times[i],'%H:%M:%S').time() == level_1_event.occured.time() and pattern.appliances[i] == level_1_event.appliance):
             result = True
-            print("Got a match!")
+            print("Matching level 1 event found in lvl 2 pattern!")
             break
     return result
 
 
-def filter_level_2_events(level_1_events: list, level_2_events: list())-> None:
-    for pattern in level_2_events:        
+def filter_level_2_events(level_1_events: list[Event], level_2_events: list[Patterns])-> list[dict]:
+    lvl_2_patterns = list()
+    for pattern in level_2_events:
+        lvl_2_dict = dict()
+        lvl_2_dict['relation'] = pattern.relation
+        lvl_2_dict['events'] = list()   
         for lvl_1_event in level_1_events: 
             if(do_events_match(pattern, lvl_1_event)):
+                lvl_2_dict['events'].append(lvl_1_event)
                 level_1_events.remove(lvl_1_event)
-    return
+        lvl_2_dict['events'].reverse()  # flip so we have expected order lvl 1 -> lvl 2
+        lvl_2_patterns.append(lvl_2_dict)
+    return lvl_2_patterns
 
